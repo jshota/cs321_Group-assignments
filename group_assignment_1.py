1a968a14fab27ffbdc8aec97c4ae462b48d1d552
Jing <jshota@hotmail.com>
2019-07-10 14:47:36 -0700

    Update group_assignment_1.py
    problem 1 fixed
    problem 2 solved

--- group_assignment_1.py
+++ group_assignment_1.py
@@ -1,19 +1,25 @@
 from typing import List
 
 class Solution:
-    def enumeration(self, l: List) -> int:
-        length = len(l)
+    def enumeration(self, l:list) -> int:
+        result_list = []
         result = 0
-        current = 0
-        for i in range(0, length):
+        for i in range(0, len(l)):
+            for j in range(i, len(l)):
+                result += l[j]
+            result_list.append(result)
+        return max(result_list)
+
+    def better_enumeration(self, l: List) -> int:
+        result = None
+        current = None
+        for i in range(0, len(l)):
             current = current + l[i] 
             if result < current or i == 0:
                 result = current
             if current < 0:
                 current = 0
         return result
-
-    
 
 s = Solution()
 s.enumeration([31,-41,59,26,-53,58,97,-93,-23,84])
