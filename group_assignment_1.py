fcaab36c5324a363220b52f2403b857f8f92d85b
Jing <jshota@hotmail.com>
2019-07-10 16:21:43 -0700

    Update group_assignment_1.py
    refined solution 2

--- group_assignment_1.py
+++ group_assignment_1.py
@@ -16,13 +16,13 @@ class Solution:
     def better_enumeration(self, l: List) -> int:
         result = 0
         current = 0
-        for i in range(0, len(l)):
-            current = current + l[i]
-            print(current)
-            if result < current or i == 0:
-                result = current
-            if current < 0:
-                current = 0
+        length = len(l)
+        for i in range(0, length):
+            current = 0
+            for j in range(i, length):
+                current = current + l[j]
+                if result < current or j == 0:
+                    result = current
         return result
 
     def dynamic(self, l:List) -> int:
@@ -36,8 +36,8 @@ class Solution:
                 result = current[j]
         return result
 
-test_list = [31,-41,59,26,-53,58,97,-93,-23,84]
+test_list = [-18, -47, -40, -43, -2, 48, 31, -24, 36, -49, 4, -29, -4, -39, 12, 24, 8, 40, 45, -17, 6, -11, -5, -30, -8, 25, -44, -9, -20, -50, -12, -32, 41, 10, -42, -15, 11, -38, 37, 21, 33, -22, 16, -41, -46, -33, -26, 7, -3, -28, 29, 20, 27, 3, 15, 49, 23, -1, 14, 32, -31, -13, -48, -14, 13, 39, 46, -35, -36, 0, 17, -27, -21, 28, -7, 44, -10, 34, -19, 47, 42, -34, 5, 26, -45, 35, 9, -25, 38, -37, -23, 22, -6, -16, 18, 43, 30, 2, 19, 1]
 s = Solution()
-s.enumeration(test_list)
-s.better_enumeration(test_list)
-s.dynamic(test_list)
+print(s.enumeration(test_list))
+print(s.better_enumeration(test_list))
+print(s.dynamic(test_list))
