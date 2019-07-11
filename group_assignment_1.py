c0c0aa61df6e56e32e9041a2eef5206b54f04b2d
Jing <jshota@hotmail.com>
2019-07-10 17:50:14 -0700

    Update group_assignment_1.py
    final version

--- group_assignment_1.py
+++ group_assignment_1.py
@@ -1,6 +1,15 @@
 from typing import List
+from time import perf_counter
+from enum import Enum
+import random
+
+class test_function (Enum):
+    enumeration = 1
+    better_enumeration = 2
+    dynamic = 3
 
 class Solution:
+
     def enumeration(self, l: List) -> int:
         result = 0
         length = len(l)
@@ -28,16 +37,40 @@ class Solution:
     def dynamic(self, l:List) -> int:
         current = [[0] * (len(l)+1) for i in range(len(l)+1)]
         current[0] = 0
+        result = 0
         for j in range(1, len(current)):
             current[j] = max(current[j-1]+l[j-1],l[j-1])
-            result = current[0]
         for j in range(1, len(current)):
             if result < current[j]:
                 result = current[j]
         return result
 
-test_list = [-18, -47, -40, -43, -2, 48, 31, -24, 36, -49, 4, -29, -4, -39, 12, 24, 8, 40, 45, -17, 6, -11, -5, -30, -8, 25, -44, -9, -20, -50, -12, -32, 41, 10, -42, -15, 11, -38, 37, 21, 33, -22, 16, -41, -46, -33, -26, 7, -3, -28, 29, 20, 27, 3, 15, 49, 23, -1, 14, 32, -31, -13, -48, -14, 13, 39, 46, -35, -36, 0, 17, -27, -21, 28, -7, 44, -10, 34, -19, 47, 42, -34, 5, 26, -45, 35, 9, -25, 38, -37, -23, 22, -6, -16, 18, 43, 30, 2, 19, 1]
+    def test(self, test_function, size_list: List, file):
+        print('Processing')
+        
+        for s in size_list:
+            for i in range(1, 10):
+                test_list = []
+                for j in range(i*s):
+                    test_list.append(random.randint(-100,100))
+                start = perf_counter()
+                if test_function == test_function.enumeration:
+                    self.enumeration(test_list)
+                elif test_function == test_function.better_enumeration:
+                    self.better_enumeration(test_list)
+                else:
+                    self.dynamic(test_list)
+                stop = perf_counter()
+                file.write('Array size: ' + str(len(test_list)) + '\n')
+                file.write('Run time: ' + str(stop - start) + ' s\n' + '****\n')
+                print('.')
+
 s = Solution()
-print(s.enumeration(test_list))
-print(s.better_enumeration(test_list))
-print(s.dynamic(test_list))
+
+fo = open("result.txt", "w")
+fo.write('better_enumeration')
+s.test(test_function.better_enumeration, [100, 1000], fo)
+
+fo.write('dynamic')
+s.test(test_function.dynamic, [100, 1000], fo)
+fo.close()
